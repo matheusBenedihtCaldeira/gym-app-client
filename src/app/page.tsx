b7da76d152107f70aa035f4c521e584ba54ff09a
matheusBenedihtCaldeira <matheusbenediht@gmail.com>
2023-09-12 15:18:29 -0300

    Update: separando navbar em

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,10 @@
-import {HomeIcon, UserIcon} from '@heroicons/react/24/solid'
-import Link from 'next/link'
+import Header from '@/components/header'
 
 export default function Home(){
     return (
-        <div className='shadow-md w-full bg-red-800 text-white'>
-            <div className='md:px-10 py-4 px-7 md:flex justify-between items-center'>
-                <div className='flex text-2xl cursor-pointer'>
-                    {/*Logo*/}
-                    <span className='font-bold'>Gym App</span>
-                </div>
-
-                {/*Nav with links*/}
-                <ul className='md:flex pl-9'>
-                    <li className='my-7 md:my-0 md:ml-8'>
-                        <Link href="/">Home</Link>
-                    </li>
-                    <li className='my-7 md:my-0 md:ml-8'>
-                        <Link href="#">Exercises</Link>
-                    </li>
-                    <li className='my-7 md:my-0 md:ml-8'>
-                        <Link href="#">Workout</Link>
-                    </li>
-                    <li className='my-7 md:my-0 md:ml-8'>
-                        <Link href="#"><UserIcon className='w-6 h-6' /></Link>
-                    </li>
-                </ul>
-            </div>
-        </div>
+        <>
+        <Header />
+        <h1>Home</h1>
+        </>
     )
 }
