77661362600e1381600ffb6a038afdc09b25cd10
matheusBenedihtCaldeira <matheusbenediht@gmail.com>
2023-09-21 15:05:45 -0300

    Personal profile

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,47 @@
+import Header from '@/components/header';
+
+export default function Profile() {
+  return (
+    <>
+      <Header />
+      <div className="m-20">
+        <div className="px-4 sm:px-0">
+          <h3 className="text-base font-semibold leading-7 text-gray-900">
+            Profile
+          </h3>
+          <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">
+            Personal informations.
+          </p>
+        </div>
+        <div className="mt-6 border-t border-gray-100">
+          <dl className="divide-y divide-gray-100">
+            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">
+                Full name
+              </dt>
+              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                Matheus Benediht Caldeira
+              </dd>
+            </div>
+            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">
+                Weight
+              </dt>
+              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                77.5
+              </dd>
+            </div>
+            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">
+                Email address
+              </dt>
+              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                matheus@gmail.com
+              </dd>
+            </div>
+          </dl>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/components/drop-down-menu.tsx
+++ src/components/drop-down-menu.tsx
@@ -9,11 +9,15 @@ export default function DropDownMenu({ isVisible }: DropDownMenuProps) {
   return (
     <div className="text-center absolute top-20 right-4 z-10 w-48 origin-top-right rounded-md text-slate-900 bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none border border-slate-200">
       <ul>
-        <Link href="#">
-          <li className="hover:bg-gray-200 p-1">Your Profile</li>
+        <Link href="profile">
+          <li className="hover:bg-gray-200 transition duration-300 ease-out md:ease-in p-1">
+            Your Profile
+          </li>
         </Link>
         <Link href="#">
-          <li className="hover:bg-gray-200 p-1">Logout</li>
+          <li className="hover:bg-gray-200 p-1 transition duration-300 ease-out md:ease-in">
+            Logout
+          </li>
         </Link>
       </ul>
     </div>
