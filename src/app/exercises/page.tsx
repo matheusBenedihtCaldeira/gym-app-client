81ab3c5254bb571f77412e31eaae6ceedeb54f2e
matheusBenedihtCaldeira <matheusbenediht@gmail.com>
2023-09-22 17:13:14 -0300

    Janela para adicionar exercicio em treino

--- src/app/exercises/page.tsx
+++ src/app/exercises/page.tsx
@@ -1,8 +1,12 @@
+'use client';
 import Header from '@/components/header';
+import WorkoutMenu from '@/components/workout-menu';
 import { PlusIcon } from '@heroicons/react/24/solid';
 import Link from 'next/link';
+import { useState } from 'react';
 
 export default function Exercises() {
+  const [showWindow, setShowWindow] = useState(false);
   return (
     <>
       <Header />
@@ -19,17 +23,48 @@ export default function Exercises() {
               </p>
             </div>
             <div className="hidden shrink-0 sm:flex sm:items-center m-2">
-              <Link
+              <button
                 className="hidden shrink-0 sm:flex sm:items-center m-2"
-                href="#"
+                onClick={() => setShowWindow(true)}
               >
                 Add to workout
                 <PlusIcon className="h-6 w-6" />
-              </Link>
+              </button>
             </div>
           </li>
         </ul>
       </div>
+      <WorkoutMenu isVisible={showWindow}>
+        <div>
+          <div className="m-4">
+            <label htmlFor="workout">Workout</label>
+          </div>
+          <div>
+            <select className="w-full h-10 rounded-lg" name="" id="">
+              <option value="">Back</option>
+              <option value="">Biceps</option>
+              <option value="">Leg</option>
+            </select>
+          </div>
+          <div className="mt-5 flex items-center justify-center">
+            <button
+              type="submit"
+              className="px-20 py-2 text-white bg-red-800 hover:bg-red-700 focus:ring focus:outline-none focus:ring-red-600 font-medium rounded-lg text-sm p-3 text-center transition duration-300 ease-out md:ease-in"
+            >
+              Add
+            </button>
+          </div>
+          <div className="mt-2 flex items-center justify-center">
+            <button
+              type="button"
+              className="rounded-md bg-gray-700 px-10 py-2 text-sm font-semibold text-white shadow-sm hover:bg-gray-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-700  transition duration-300 ease-out md:ease-in"
+              onClick={() => setShowWindow(false)}
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
+      </WorkoutMenu>
     </>
   );
 }
--- src/components/workout-menu.tsx
+++ src/components/workout-menu.tsx
@@ -1,15 +1,13 @@
 import { ReactNode } from 'react';
 
-interface RegisterWorkoutWindowProps {
+interface WorkoutMenuProps {
   isVisible: boolean;
   children: ReactNode;
 }
 
-export default function RegisterWorkoutWindow({
-  isVisible,
-  children,
-}: RegisterWorkoutWindowProps) {
+export default function WorkoutMenu({ isVisible, children }: WorkoutMenuProps) {
   if (!isVisible) return null;
+
   return (
     <div className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center">
       <div className="w-[600px] h-[500px] ">
